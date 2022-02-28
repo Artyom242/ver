06e5a9e2af1ab55db99de91b803111f24ac2b929
Artyom242 <97839696+Artyom242@users.noreply.github.com>
2022-02-28 21:54:14 +0700

    Add files via upload

---
+++ ter/js/script.js
@@ -0,0 +1,10 @@
+menu.onclick = fuction myFuction() {
+	var x = document.getElementById('myTopnav');
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+
+}
